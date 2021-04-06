15d93e902cd5d1235cf36ce9ea46de08eda8fd59
Mario <mario@die-davids.de>
2021-04-06 21:48:58 +0200

    README

---
+++ README.md
@@ -0,0 +1,7 @@
+# CUBA Platform Example - SaaS Management
+
+This example shows you how to manage SaaS subscriptions as part of the CUBA application.
+
+## Entity Model
+
+![entity model](img/entity-model.png)
--- modules/core/test/de/diedavids/cuba/ccsm/CcsmTestContainer.java
+++ modules/core/test/de/diedavids/cuba/ccsm/CcsmTestContainer.java
@@ -14,9 +14,9 @@ public class CcsmTestContainer extends TestContainer {
     public CcsmTestContainer() {
         super();
         appComponents = Arrays.asList(
-            "com.haulmont.cuba",
-            "com.haulmont.addon.sdbmt",
-            "com.haulmont.addon.restapi");
+                "com.haulmont.cuba",
+                "com.haulmont.addon.sdbmt",
+                "com.haulmont.addon.restapi");
         appPropertiesFiles = Arrays.asList(
                 // List the files defined in your web.xml
                 // in appPropertiesConfig context parameter of the core module
